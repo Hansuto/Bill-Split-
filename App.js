f4b4fc8e85de77e2b75da188ad98d899f52e7c66
Christopher <guitartaliaferro@yahoo.com>
2019-02-24 09:07:35 -0500

    added some initial styling

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Picker } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Picker, TouchableOpacity } from 'react-native';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import Animation from 'lottie-react-native';
+import { Font } from 'expo';
 import './global.js'
 console.disableYellowBox = true;
 
 export class StartScreen extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  } 
+
+  async componentDidMount () {
+    await Font.loadAsync({
+      'Bebas': require('./BebasKai.ttf'),
+    });
+    this.setState({ isLoading: false })
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.container}/>
+      );
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Bill Split!</Text>
-        <Button
-          onPress={() => this.props.navigation.navigate('getPeople')}
-          title="Lets Start"
-        />
+        <Text style={{fontFamily: 'Bebas', fontSize: 100, color: '#3A972D', marginBottom: 250}}>Bill Split!</Text>
+        <Animation source={require('./animation-w2835-h1701.json')} autoPlay/>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('getPeople')}>
+            <Text style={{fontSize:70, fontFamily:'Bebas', color: '#3A972D', textAlign:'center'}}>Press Here</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -22,19 +45,20 @@ export class GetPeopleScreen extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text>How many roommates?</Text>
-        <View style={{flexDirection : 'row'}}>
-          <View style={{backgroundColor: '#fff', marginRight: 10, width: 100}}>
-            <TextInput
-              keyboardType='numeric'
-              onChangeText={(text) => numPeople = text}
-            />
-          </View>
-          <Button
-            onPress={() => this.props.navigation.navigate('getNames')}
-            title="Next"
+        <Text style={{ fontFamily: 'Bebas', fontSize: 50, color: '#3A972D', textAlign: 'center'}}>How many roommates do you have?</Text>
+        <View style={{ backgroundColor: '#fff', marginVertical: 20, width: 100, height: 100, elevation: 5, borderRadius: 20}}>
+          <TextInput
+            textAlign= 'center'
+            caretHidden= {true}
+            maxLength={2}
+            style={{flex:1, fontFamily: 'Bebas', fontSize: 50, color: '#3A972D',}}
+            keyboardType='numeric'
+            onChangeText={(text) => numPeople = text}
           />
         </View>
+        <TouchableOpacity onPress={() => this.props.navigation.navigate('getNames')}>
+          <Text style={{fontSize:70, fontFamily:'Bebas', color: '#3A972D', textAlign:'center'}}>➭</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -62,21 +86,22 @@ export class GetNamesScreen extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text>What are their names?</Text>
+        <Text style={{ fontFamily: 'Bebas', fontSize: 50, color: '#3A972D', textAlign: 'center', marginHorizontal:15}}>What are their names?</Text>
         <View style={{marginVertical:10}}>
-          <Text>Roommate {this.state.index + 1}</Text>
-          <View style={{flexDirection: 'row'}}>
-            <View style={{backgroundColor: '#fff', marginRight: 10, width: 100}}> 
-              <TextInput
-                onChangeText={(text) => this.setState({nameEntry: text})}
-                ref={input => {this.textInput = input}}
-              />
-            </View>
-            <Button
-              onPress={() => {this.submitName()}}
-              title="Next"
+          <Text style={{fontFamily: 'Bebas', fontSize: 20, color: '#3A972D', textAlign: 'center'}}>Roommate {this.state.index + 1}</Text>
+          <View style={{ backgroundColor: '#fff', marginVertical: 20, width: 300, height: 70, elevation: 5, borderRadius: 20}}>
+            <TextInput
+              textAlign= 'center'
+              caretHidden= {true}
+              maxLength={12}
+              style={{flex:1, fontFamily: 'Bebas', fontSize: 50, color: '#3A972D',}}
+              onChangeText={(text) => this.setState({nameEntry: text})}
+              ref={input => {this.textInput = input}}
             />
-          </View>
+          </View>  
+          <TouchableOpacity onPress={() => {this.submitName()}}>
+            <Text style={{fontSize:70, fontFamily:'Bebas', color: '#3A972D', textAlign:'center'}}>➭</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -86,21 +111,22 @@ export class GetNamesScreen extends React.Component{
 export class GetBillsScreen extends React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text>How many bills?</Text>
-        <View style={{flexDirection : 'row'}}>
-          <View style={{backgroundColor: '#fff', marginRight: 10, width: 100}}>
-            <TextInput
-              keyboardType='number-pad'
-              onChangeText={(text) => numBills = text}
-            />
-          </View>
-          <Button
-            onPress={() => this.props.navigation.navigate('getBillDetails')}
-            title="Next"
-          />
-        </View>
+     <View style={styles.container}>
+      <Text style={{ fontFamily: 'Bebas', fontSize: 50, color: '#3A972D', textAlign: 'center'}}>How many bills?</Text>
+      <View style={{ backgroundColor: '#fff', marginVertical: 20, width: 100, height: 100, elevation: 5, borderRadius: 20}}>
+        <TextInput
+          textAlign= 'center'
+          caretHidden= {true}
+          maxLength={2}
+          style={{flex:1, fontFamily: 'Bebas', fontSize: 50, color: '#3A972D',}}
+          keyboardType='numeric'
+          onChangeText={(text) => numBills = text}
+        />
       </View>
+      <TouchableOpacity onPress={() => this.props.navigation.navigate('getBillDetails')}>
+        <Text style={{fontSize:70, fontFamily:'Bebas', color: '#3A972D', textAlign:'center'}}>➭</Text>
+      </TouchableOpacity>
+    </View>
     );
   }
 }
@@ -130,27 +156,33 @@ export class GetBillDetails extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text>What are they?</Text>
         <View style={{marginVertical:10}}>
-          <Text>Bill {this.state.index + 1}</Text>
-          <View style={{backgroundColor: '#fff', width: 100}}> 
+          <Text style={{ fontFamily: 'Bebas', fontSize: 30, color: '#3A972D', textAlign: 'center'}}>Bill {this.state.index + 1} Name</Text>
+          <View style={{ backgroundColor: '#fff', marginTop: 5, marginBottom: 20, width: 300, height: 70, elevation: 5, borderRadius: 20}}>
             <TextInput
+              textAlign= 'center'
+              caretHidden= {true}
+              maxLength={12}
+              style={{flex:1, fontFamily: 'Bebas', fontSize: 50, color: '#3A972D',}}
               onChangeText={(text) => this.setState({nameEntry: text})}
               ref={input => {this.textInput = input}}
             />
           </View>
-          <Text>Price</Text>
-          <View style={{backgroundColor: '#fff', marginBottom:10, width: 100}}> 
+          <Text style={{ fontFamily: 'Bebas', fontSize: 30, color: '#3A972D', textAlign: 'center'}}>Price</Text>
+          <View style={{ backgroundColor: '#fff', marginTop: 5, marginBottom: 20, width: 300, height: 70, elevation: 5, borderRadius: 20}}>
             <TextInput
+              textAlign= 'center'
+              caretHidden= {true}
+              maxLength={7}
               keyboardType='number-pad'
+              style={{flex:1, fontFamily: 'Bebas', fontSize: 50, color: '#3A972D',}}
               onChangeText={(num) => this.setState({price: num})}
               ref={input => {this.textInput2 = input}}
             />
           </View>
-          <Button
-              onPress={() => {this.submitBill()}}
-              title="Next"
-          />
+          <TouchableOpacity onPress={() => {this.submitBill()}}>
+            <Text style={{fontSize:70, fontFamily:'Bebas', color: '#3A972D', textAlign:'center'}}>➭</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -178,20 +210,19 @@ export class GetAssignScreen extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text>Who pays {billNames[this.state.index]}?</Text>
+        <Text style={{ fontFamily: 'Bebas', fontSize: 50, color: '#3A972D', textAlign: 'center', marginHorizontal: 15}}>Who pays for {billNames[this.state.index]}?</Text>
         <Picker
-        style={{width: 150, height:180}}
-        selectedValue={this.state.selected}
-        itemStyle={{color:'#fff', fontSize:26}}
-        onValueChange={(index) => this.setState({selected: index})}>
-          {names.map((value, i) => (
-            <Picker.Item label={value} value={i} key={i}/>
-          ))}
+          style={{width: 150, height:180, fontFamily: 'Bebas', fontSize: 30, color: '#3A972D', textAlign: 'center'}}
+          selectedValue={this.state.selected}
+          itemStyle={{color:'#fff', fontSize:26}}
+          onValueChange={(index) => this.setState({selected: index})}>
+            {names.map((value, i) => (
+              <Picker.Item label={value} value={i} key={i}/>
+            ))}
         </Picker>
-        <Button
-          onPress={() => this.assignToBill()}
-          title="Next"
-        />
+        <TouchableOpacity onPress={() => this.assignToBill()}>
+            <Text style={{fontSize:70, fontFamily:'Bebas', color: '#3A972D', textAlign:'center'}}>➭</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -259,7 +290,7 @@ export class ResultsScreen extends React.Component{
         if(result[i][j] > 0.00){
           resultComponent.push(
             <View>
-              <Text>{names[i]} owes {names[j]} ${result[i][j].toFixed(2)}</Text>
+              <Text style={{ fontFamily: 'Bebas', fontSize: 30, color: '#3A972D', textAlign: 'center', marginHorizontal: 15}}>{names[i]} owes {names[j]} ${result[i][j].toFixed(2)}</Text>
             </View>
           )
         }
@@ -283,12 +314,11 @@ export class ResultsScreen extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text>RESULTS!</Text>
+        <Text style={{ fontFamily: 'Bebas', fontSize: 70, color: '#3A972D', textAlign: 'center', marginHorizontal: 15}}>RESULTS</Text>
         {resultComponent}
-        <Button
-          onPress={() => this.reset()}
-          title="Lets do that again!"
-        />
+        <TouchableOpacity onPress={() => this.reset()}>
+            <Text style={{fontSize:50, fontFamily:'Bebas', color: '#3A972D', textAlign:'center', marginTop:30}}>Start Over</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -311,7 +341,7 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#7D7D7D',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
